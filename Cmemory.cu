4cfdf903e00c317c65debe738d52caeb31d8754c
Cooper Liu <2044199378@qq.com>
2017-11-17 20:36:56 +0800

    核心代码forked from iSawyer，主程序仅调用和测试

---
+++ Cmemory.cu
@@ -0,0 +1,133 @@
+#include "cmemory.h"
+#include <cstring>
+#include <iostream>
+//#include <memory>
+//#include "common.h"
+#include "cuda_runtime.h"
+//#include "math_functions.h"
+
+void main()
+{
+	size_t n = 10;
+	int *b = (int *)malloc(10 * sizeof(int));
+	int *c = (int *)malloc(10 * sizeof(int));
+	for (int i = 0; i < 10; i++)
+		b[i] = i + 1;
+	for (int i = 0; i < 10; i++)
+		c[i] = 1;
+	Cmemory a=Cmemory(n);
+	a.cpu_data();
+	a.set_cpu_data(b);
+
+	for (int i = 0; i < 10; i++)
+		printf("%d ", c[i]);
+	printf("\n");
+
+	c = (int *)a.mutable_cpu_data();
+
+	for (int i = 0; i < 10; i++)
+		printf("%d ", c[i]);
+	printf("ok");
+	system("pause");
+}
+
+Cmemory::~Cmemory(){
+	if (cpu_ptr && own_cpu_data){
+		Cfree(cpu_ptr);
+	}
+
+	if (gpu_ptr){
+		cudaFree(gpu_ptr);
+	}
+
+}
+
+
+inline void Cmemory::to_cpu(){
+	switch (_head){
+
+	case UNINITIALIZED:
+		Cmalloc(&cpu_ptr, size_);
+		// biao zhun ku,  meiyou zuo wapper
+		memset(cpu_ptr, 0, size_);
+
+		_head = HEAD_AT_CPU;
+		own_cpu_data = true;
+		break;
+
+	case HEAD_AT_GPU:
+		if (cpu_ptr){
+			// use C_gpu_memcpy in math.cu
+			//c_copy(size_,gpu_ptr,cpu_ptr);
+			cudaMemcpy(cpu_ptr, gpu_ptr, size_, cudaMemcpyDefault);
+		}
+		else{
+			Cmalloc(&cpu_ptr, size_);
+			//c_copy(size_,gpu_ptr,cpu_ptr);
+			cudaMemcpy(cpu_ptr, gpu_ptr, size_, cudaMemcpyDefault);
+			own_cpu_data = true;
+		}
+		_head = SYNCED;
+		break;
+
+	case HEAD_AT_CPU:
+	case SYNCED:
+		break;
+
+	}
+}
+
+inline void Cmemory::to_gpu(){
+
+	switch (_head){
+	case UNINITIALIZED:
+		cudaMalloc(&gpu_ptr, size_);
+		_head = HEAD_AT_GPU;
+		break;
+	case HEAD_AT_CPU:
+		if (gpu_ptr == NULL){
+			cudaMalloc(&gpu_ptr, size_);
+		}
+		cudaMemcpy(gpu_ptr, cpu_ptr, size_, cudaMemcpyDefault);
+		//c_copy(size_,cpu_ptr,gpu_ptr);
+		_head = SYNCED;
+		break;
+	case HEAD_AT_GPU:
+	case SYNCED:
+		break;
+	}
+
+}
+
+const void* Cmemory::cpu_data(){
+	to_cpu();
+	return (const void*)cpu_ptr;
+}
+
+const void* Cmemory::gpu_data(){
+	to_gpu();
+	return (const void*)gpu_ptr;
+}
+
+void* Cmemory::mutable_cpu_data(){
+	to_cpu();
+	_head = HEAD_AT_CPU;
+	return cpu_ptr;
+}
+
+void* Cmemory::mutable_gpu_data(){
+	to_gpu();
+	_head = HEAD_AT_GPU;
+	return gpu_ptr;
+}
+
+void Cmemory::set_cpu_data(void* data){
+	if (own_cpu_data){
+		Cfree(cpu_ptr);
+	}
+	cpu_ptr = data;
+	_head = HEAD_AT_CPU;
+	own_cpu_data = false;
+}
+
+
